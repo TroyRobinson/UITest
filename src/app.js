141f8e9677d43ca9617cbd78f6063965f65af106
Troy Robinson <troy@lionsrock.com>
2024-12-11 15:04:29 -0600

    2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import '../public/globals.css'
 import { FlexCol } from './utils'
+import { Group } from 'utopia-api'
 
 export var App = () => {
   return (
@@ -9,19 +10,47 @@ export var App = () => {
         width: '100%',
         height: '100%',
         background: 'white',
-
         justifyContent: 'center',
         alignItems: 'center',
+        flexDirection: 'row',
       }}
     >
-      <img
-        src='https://github.com/concrete-utopia/utopia/blob/master/editor/resources/editor/pyramid_fullsize@2x.png?raw=true'
-        alt='Utopia logo'
+      <div
         style={{
-          width: 357,
-          height: 453,
+          contain: 'layout',
+          width: 'max-content',
+          height: 'max-content',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'flex-start',
         }}
-      ></img>
+      >
+        <span>Title</span>
+        <button
+          style={{
+            width: 130,
+            height: 58,
+            contain: 'layout',
+            backgroundColor: 'rgb(255, 164, 164, 1)',
+          }}
+        />
+        <button
+          style={{
+            width: 130,
+            height: 58,
+            contain: 'layout',
+            backgroundColor: 'rgb(255, 164, 164, 1)',
+          }}
+        />
+        <button
+          style={{
+            width: 130,
+            height: 58,
+            contain: 'layout',
+          }}
+        />
+      </div>
     </FlexCol>
   )
 }
