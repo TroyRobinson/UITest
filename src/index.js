9674bfd6b4e05b1ff398c007d2f7c2de66c94be5
Troy Robinson <troy@lionsrock.com>
2024-12-11 18:05:30 -0600

    Update app to use Radix component library
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/TroyRobinson/UITest?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import { App } from '../src/app'
+import { Box } from '@radix-ui/react'
 
 const root = document.getElementById('root')
 if (root != null) {
-  ReactDOM.render(<App />, root)
+  ReactDOM.render(
+    <Box>
+      <App />
+    </Box>,
+    root
+  )
 }
